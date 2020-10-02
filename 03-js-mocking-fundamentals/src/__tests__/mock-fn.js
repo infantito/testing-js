e174e8a19d1a6f963844d4003a17c37f95fe2b4c
infantito <danielinfantito61@gmail.com>
2020-10-01 20:44:47 -0500

    feat: mock called correctly

--- 03-js-mocking-fundamentals/src/__tests__/mock-fn.js
+++ 03-js-mocking-fundamentals/src/__tests__/mock-fn.js
@@ -14,20 +14,40 @@
  * Execute: Use `npx jest --watch src/__tests__/mock-fn.js` to watch the test
  */
 
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const thumbWar = require('../thumb-war');
+const utils = require('../utils');
 
 test('returns winner', () => {
-  const originalGetWinner = utils.getWinner
-  utils.getWinner = jest.fn((p1, p2) => p1)
+  const originalGetWinner = utils.getWinner;
+  utils.getWinner = jest.fn((p1, p2) => p1);
 
-  const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-  expect(winner).toBe('Kent C. Dodds')
+  const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler');
+  expect(winner).toBe('Kent C. Dodds');
   // Your code:
+  // from
+  /**
+    expect(utils.getWinner).toHaveBeenCalledTimes(2);
+    expect(utils.getWinner).toHaveBeenCalledWith('Kent C. Dodds', 'Ken Wheeler');
+    expect(utils.getWinner).toHaveBeenNthCalledWith(
+      1,
+      'Kent C. Dodds',
+      'Ken Wheeler',
+    );
+    expect(utils.getWinner).toHaveBeenNthCalledWith(
+      2,
+      'Kent C. Dodds',
+      'Ken Wheeler',
+    );
+   */
+  // to
+  expect(utils.getWinner.mock.calls).toEqual([
+    ['Kent C. Dodds', 'Ken Wheeler'],
+    ['Kent C. Dodds', 'Ken Wheeler'],
+  ]);
 
   // cleanup
-  utils.getWinner = originalGetWinner
-})
+  utils.getWinner = originalGetWinner;
+});
 
 /**
  * Hints:
--- 03-js-mocking-fundamentals/src/no-framework/mock-fn.js
+++ 03-js-mocking-fundamentals/src/no-framework/mock-fn.js
@@ -6,24 +6,35 @@
  * Execute: Use `npx jest --watch src/no-framework/mock-fn.js` to watch the test
  */
 
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const assert = require('assert');
+const thumbWar = require('../thumb-war');
+const utils = require('../utils');
 
 // Your Code:
+function fn(impl) {
+  const mockFn = (...args) => {
+    mockFn.mock.calls.push(args);
 
-const originalGetWinner = utils.getWinner
-utils.getWinner = fn((p1, p2) => p1)
+    return impl(...args);
+  };
 
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+  mockFn.mock = {calls: []};
+
+  return mockFn;
+}
+
+const originalGetWinner = utils.getWinner;
+utils.getWinner = fn((p1, p2) => p1);
+
+const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler');
+assert.strictEqual(winner, 'Kent C. Dodds');
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
   ['Kent C. Dodds', 'Ken Wheeler'],
   ['Kent C. Dodds', 'Ken Wheeler'],
-])
+]);
 
 // cleanup
-utils.getWinner = originalGetWinner
+utils.getWinner = originalGetWinner;
 
 /**
  * Checkout master branch to see the answer.
