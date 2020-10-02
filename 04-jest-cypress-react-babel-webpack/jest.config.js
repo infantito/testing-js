1499ab1767bca449a1c7f2fb2313664ec86ec2b2
infantito <danielinfantito61@gmail.com>
2020-10-01 23:02:06 -0500

    feat: support using webpack css modules with jest

--- 04-jest-cypress-react-babel-webpack/jest.config.js
+++ 04-jest-cypress-react-babel-webpack/jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
+    /**
+     * It's actually important that this shows up before the `.css`
+     * because order does matter here and this would match before
+     * this gets a chance to test and match.
+     * We want this to happen first and for any other CSS files in
+     * our project, we'll just do the regular style mode.
+     */
+    '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
 }
