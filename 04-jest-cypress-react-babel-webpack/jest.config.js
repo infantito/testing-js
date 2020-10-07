90a6163a5fd58c404fd8c9330c28a28873039473
infantito <danielinfantito61@gmail.com>
2020-10-07 12:39:30 -0500

    chore: set a code coverage threshold in jest to maintain code coverage levels

--- 04-jest-cypress-react-babel-webpack/jest.config.js
+++ 04-jest-cypress-react-babel-webpack/jest.config.js
@@ -22,4 +22,18 @@ module.exports = {
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   snapshotSerializers: ['jest-emotion'],
   collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 31,
+      branches: 18,
+      functions: 29,
+      lines: 29,
+    },
+    './src/shared/utils.js': {
+      statements: 100,
+      branches: 80,
+      functions: 100,
+      lines: 100,
+    },
+  },
 }
