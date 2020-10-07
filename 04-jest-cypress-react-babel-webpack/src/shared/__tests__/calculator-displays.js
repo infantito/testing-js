e5c84767bb989da4faf23d73162a1a5ae7a9c01b
infantito <danielinfantito61@gmail.com>
2020-10-06 20:56:48 -0500

    feat: test with providers as theme - react router or redux

--- 04-jest-cypress-react-babel-webpack/src/shared/__tests__/calculator-displays.js
+++ 04-jest-cypress-react-babel-webpack/src/shared/__tests__/calculator-displays.js
@@ -1,24 +1,37 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import {render} from '@testing-library/react'
+import {ThemeProvider} from 'emotion-theming'
+import {dark} from '../../themes'
 import CalculatorDisplay from '../calculator-display'
 
+function Wrapper({children}) {
+  return <ThemeProvider theme={dark}>{children}</ThemeProvider>
+}
+
+Wrapper.propTypes = {
+  children: PropTypes.node,
+}
+
 test('renders', () => {
-  const {container} = render(<CalculatorDisplay value="0" />)
+  const {container} = render(<CalculatorDisplay value="0" />, {
+    wrapper: Wrapper,
+  })
 
   expect(container.firstChild).toMatchInlineSnapshot(`
     .emotion-0 {
       position: relative;
-      color: white;
-      background: #1c191c;
       line-height: 130px;
       font-size: 6em;
       -webkit-flex: 1;
       -ms-flex: 1;
       flex: 1;
+      color: white;
+      background: #1c191c;
     }
 
     <div
-      class="emotion-0"
+      class="emotion-0 emotion-1"
     >
       <div
         class="autoScalingText"
