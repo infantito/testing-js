774371610e6fd1a9ef882f8369ba5ae250ebb48c
infantito <danielinfantito61@gmail.com>
2020-10-08 22:46:23 -0500

    refactor: remove debug statement

--- 05-react-testing-library-course/src/__tests__/react-dom.js
+++ 05-react-testing-library-course/src/__tests__/react-dom.js
@@ -2,13 +2,10 @@ import React from 'react'
 import {render, screen} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
-test('renders a number input with a label "Favorite Number"', async () => {
-  const {debug} = await render(<FavoriteNumber />)
+test('renders a number input with a label "Favorite Number"', () => {
+  render(<FavoriteNumber />)
 
   const input = screen.getByLabelText(/favorite number/i)
 
   expect(input).toHaveAttribute('type', 'number')
-
-  // print html
-  debug(input)
 })
