3afe61e1289ac42b683ed24a18dedc3bd82315aa
infantito <danielinfantito61@gmail.com>
2020-10-08 22:45:47 -0500

    feat: debug the dom state test using react testing library's debug function

--- 05-react-testing-library-course/src/__tests__/react-dom.js
+++ 05-react-testing-library-course/src/__tests__/react-dom.js
@@ -2,10 +2,13 @@ import React from 'react'
 import {render, screen} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
-test('renders a number input with a label "Favorite Number"', () => {
-  render(<FavoriteNumber />)
+test('renders a number input with a label "Favorite Number"', async () => {
+  const {debug} = await render(<FavoriteNumber />)
 
   const input = screen.getByLabelText(/favorite number/i)
 
   expect(input).toHaveAttribute('type', 'number')
+
+  // print html
+  debug(input)
 })
