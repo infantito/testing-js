4d89d9fbad2ab3fc8f080556522ac22325e7c527
infantito <danielinfantito61@gmail.com>
2020-10-09 12:52:38 -0500

    refactor: test prop updates with rtl

--- 05-react-testing-library-course/src/__tests__/state.js
+++ 05-react-testing-library-course/src/__tests__/state.js
@@ -3,12 +3,18 @@ import user from '@testing-library/user-event'
 import {render, screen} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
-test('entering an invalid value shows an error message', () => {
-  render(<FavoriteNumber />)
+test('entering an invalid value shows an error message', async () => {
+  const {rerender, debug} = await render(<FavoriteNumber />)
 
   const input = screen.getByLabelText(/favorite number/i)
 
   user.type(input, '10')
 
   expect(screen.getByRole('alert')).toHaveTextContent(/the number is invalid/i)
+
+  debug()
+
+  rerender(<FavoriteNumber max={10} />)
+
+  debug()
 })
