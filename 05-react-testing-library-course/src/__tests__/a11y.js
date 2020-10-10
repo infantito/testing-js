df49c83f03e66912aa2a48ff2297159b0fe9d700
infantito <danielinfantito61@gmail.com>
2020-10-10 12:53:05 -0500

    feat: test accessibility of rendered react components with jest-axe

---
+++ 05-react-testing-library-course/src/__tests__/a11y.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+import {axe} from 'jest-axe'
+
+function Form() {
+  return (
+    <form>
+      <label htmlFor="email">Email</label>
+      <input type="email" id="email" placeholder="email" />
+    </form>
+  )
+}
+
+test('the form is accessible', async () => {
+  const {container} = render(<Form />)
+
+  const results = await axe(container)
+
+  expect(results).toHaveNoViolations()
+})
