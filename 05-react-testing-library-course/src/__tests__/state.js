56058c6f651d9dfb293b27d56f1879b9baeec052
infantito <danielinfantito61@gmail.com>
2020-10-10 12:37:05 -0500

    refactor: asert that somenthing is not rendered

--- 05-react-testing-library-course/src/__tests__/state.js
+++ 05-react-testing-library-course/src/__tests__/state.js
@@ -4,17 +4,17 @@ import {render, screen} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
 test('entering an invalid value shows an error message', async () => {
-  const {rerender, debug} = await render(<FavoriteNumber />)
+  const {rerender} = await render(<FavoriteNumber />)
 
   const input = screen.getByLabelText(/favorite number/i)
 
   user.type(input, '10')
 
-  expect(screen.getByRole('alert')).toHaveTextContent(/the number is invalid/i)
-
-  debug()
+  expect(screen.queryByRole('alert')).toHaveTextContent(
+    /the number is invalid/i,
+  )
 
   rerender(<FavoriteNumber max={10} />)
 
-  debug()
+  expect(screen.queryByRole('alert')).toBeNull()
 })
