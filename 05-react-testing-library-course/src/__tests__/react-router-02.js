e132738ba53e98fa4a8c379775b314b0de947a42
infantito <danielinfantito61@gmail.com>
2020-10-15 21:49:42 -0500

    feat: initialize with a bad entry to test the react-router-dom no-match route

---
+++ 05-react-testing-library-course/src/__tests__/react-router-02.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {BrowserRouter} from 'react-router-dom'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {Main} from '../main'
+
+test('main renders about and home and I can navigate to those pages', () => {
+  window.history.pushState({}, 'Test page', '/')
+
+  render(
+    <BrowserRouter>
+      <Main />
+    </BrowserRouter>,
+  )
+
+  /**
+   * https://testing-library.com/docs/dom-testing-library/api-queries#level
+   * The level option queries the element(s) with the heading role matching
+   * the indicated level determined by the semantic HTML heading elements
+   * <h1>-<h6> or matching the aria-level attribute.
+   */
+  expect(screen.getByRole('heading')).toHaveTextContent(/home/i)
+
+  userEvent.click(screen.getByText(/about/i))
+
+  expect(screen.getByRole('heading')).toHaveTextContent(/about/i)
+})
+
+test('landing on a bad page shows no match component', () => {
+  window.history.pushState({}, 'Test page', '/something-that-does-not-match')
+
+  render(
+    <BrowserRouter>
+      <Main />
+    </BrowserRouter>,
+  )
+
+  expect(screen.getByRole('heading')).toHaveTextContent(/404/i)
+})
