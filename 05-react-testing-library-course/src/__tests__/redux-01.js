aabff1536339f7c0824ef9ac00519a9de0b8a5d4
infantito <danielinfantito61@gmail.com>
2020-10-15 22:18:05 -0500

    feat: test a redux connected react component

---
+++ 05-react-testing-library-course/src/__tests__/redux-01.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Provider} from 'react-redux'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {Counter} from '../redux-counter'
+import {store} from '../redux-store'
+
+test('can render with redux with defaults', () => {
+  render(
+    <Provider store={store}>
+      <Counter />
+    </Provider>,
+  )
+
+  userEvent.click(screen.getByText('+'))
+
+  expect(screen.getByLabelText(/count/i)).toHaveTextContent('1')
+})
