030f2f29734eb79fd47d7453cf2bcd124e802d46
infantito <danielinfantito61@gmail.com>
2020-10-25 19:35:31 -0500

    feat: write starting out app 03

---
+++ 05-react-testing-library-course/src/__tests__/app-03.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {render, fireEvent, screen} from '@testing-library/react'
+import {submitForm as mockSubmitForm} from '../api'
+import App from '../app'
+
+jest.mock('../api')
+
+test('Can fill out a form across multiple pages', async () => {
+  mockSubmitForm.mockResolvedValueOnce({success: true})
+
+  const testData = {food: 'test food', drink: 'test drink'}
+
+  render(<App />)
+
+  fireEvent.click(await screen.findByText(/fil.*form/i))
+
+  fireEvent.change(await screen.findByLabelText(/food/i), {
+    target: {value: testData.food},
+  })
+
+  fireEvent.click(await screen.findByText(/next/i))
+
+  fireEvent.change(await screen.findByLabelText(/drink/i), {
+    target: {value: testData.drink},
+  })
+
+  fireEvent.click(await screen.findByText(/review/i))
+
+  expect(await screen.findByLabelText(/food/i)).toHaveTextContent(testData.food)
+
+  expect(await screen.findByLabelText(/drink/i)).toHaveTextContent(
+    testData.drink,
+  )
+
+  fireEvent.click(await screen.findByText(/confirm/i, {selector: 'button'}))
+
+  expect(mockSubmitForm).toHaveBeenCalledWith(testData)
+
+  expect(mockSubmitForm).toHaveBeenCalledTimes(1)
+
+  fireEvent.click(await screen.findByText(/home/i))
+
+  expect(await screen.findByText(/welcome home/i)).toBeInTheDocument()
+})
