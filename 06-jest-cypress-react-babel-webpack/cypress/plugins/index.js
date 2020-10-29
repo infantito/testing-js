190f1675616f6643e84ddd0682b76ad102f2fde9
infantito <danielinfantito61@gmail.com>
2020-10-28 21:17:17 -0500

    fix: disable eslint for cypress plugin

--- 06-jest-cypress-react-babel-webpack/cypress/plugins/index.js
+++ 06-jest-cypress-react-babel-webpack/cypress/plugins/index.js
@@ -12,10 +12,11 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+/*eslint valid-jsdoc: "off"*/
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (on, config) => {
+module.exports = (_on, _config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
