35e3fb9c5221705f8aeb259706cf86568ccf89de
infantito <danielinfantito61@gmail.com>
2020-11-07 18:26:59 -0500

    chore: update components with registration

--- 06-jest-cypress-react-babel-webpack/src/shared/__tests__/calculator-display.js
+++ 06-jest-cypress-react-babel-webpack/src/shared/__tests__/calculator-display.js
@@ -1,33 +1,8 @@
 import * as React from 'react'
 import {render} from 'calculator-test-utils'
-import {dark} from 'themes'
 import CalculatorDisplay from '../calculator-display'
 
 test('renders', () => {
-  const {container} = render(<CalculatorDisplay value="0" />, {theme: dark})
-
-  expect(container.firstChild).toMatchInlineSnapshot(`
-    .emotion-0 {
-      position: relative;
-      line-height: 130px;
-      font-size: 6em;
-      -webkit-flex: 1;
-      -ms-flex: 1;
-      flex: 1;
-      color: white;
-      background: #1c191c;
-    }
-
-    <div
-      class="emotion-0 emotion-1"
-    >
-      <div
-        class="autoScalingText"
-        data-testid="total"
-        style="transform: scale(1,1);"
-      >
-        0
-      </div>
-    </div>
-  `)
+  const {container} = render(<CalculatorDisplay value="0" />)
+  expect(container.firstChild).toMatchSnapshot()
 })
