695786500c77338cffdb7b693a36db676768a0b3
infantito <danielinfantito61@gmail.com>
2020-11-07 20:23:53 -0500

    feat: run tests as an authenticated user with cypress

--- 06-jest-cypress-react-babel-webpack/cypress/e2e/calculator.js
+++ 06-jest-cypress-react-babel-webpack/cypress/e2e/calculator.js
@@ -14,3 +14,25 @@ describe('anonymous calculator', () => {
     cy.findByTestId('total').should('have.text', '3')
   })
 })
+
+describe('authenticated calculator', () => {
+  it('displays the username', () => {
+    cy.createUser().then(user => {
+      cy.visit('/')
+
+      cy.findByText(/login/i).click()
+
+      cy.findByLabelText(/username/i).type(user.username)
+
+      cy.findByLabelText(/password/i).type(user.password)
+
+      cy.findByText(/submit/i).click()
+
+      cy.findByTestId('username-display').should('have.text', user.username)
+
+      cy.findByText(/logout/i).click()
+
+      cy.findByTestId('username-display').should('not.exist')
+    })
+  })
+})
