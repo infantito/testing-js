b8e74f59b84133933254e8e51272334ce64993d8
infantito <danielinfantito61@gmail.com>
2020-11-07 20:04:29 -0500

    feat: keep tests isolated and focused with custom cypress commands

--- 06-jest-cypress-react-babel-webpack/cypress/e2e/login.js
+++ 06-jest-cypress-react-babel-webpack/cypress/e2e/login.js
@@ -1,33 +1,24 @@
-import {userBuilder} from '../support/generate'
-
 describe('login', () => {
   it('should register a new user', () => {
-    // create user
-    const user = userBuilder()
+    cy.createUser().then(user => {
+      cy.visit('/')
 
-    cy.request({
-      url: 'http://localhost:3000/register',
-      method: 'POST',
-      body: user,
+      cy.findByText(/login/i).click()
+
+      cy.findByLabelText(/username/i).type(user.username)
+
+      cy.findByLabelText(/password/i).type(user.password)
+
+      cy.findByText(/submit/i).click()
+
+      // now let's verify things are set after login
+      cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+
+      cy.window()
+        .its('localStorage.token')
+        .should('be.a', 'string')
+
+      cy.findByTestId('username-display').should('have.text', user.username)
     })
-
-    cy.visit('/')
-
-    cy.findByText(/login/i).click()
-
-    cy.findByLabelText(/username/i).type(user.username)
-
-    cy.findByLabelText(/password/i).type(user.password)
-
-    cy.findByText(/submit/i).click()
-
-    // now let's verify things are set after login
-    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
-
-    cy.window()
-      .its('localStorage.token')
-      .should('be.a', 'string')
-
-    cy.findByTestId('username-display').should('have.text', user.username)
   })
 })
--- 06-jest-cypress-react-babel-webpack/cypress/support/commands.js
+++ 06-jest-cypress-react-babel-webpack/cypress/support/commands.js
@@ -1,25 +1,12 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+import {buildUser} from '../support/generate'
+
+Cypress.Commands.add('createUser', overrides => {
+  // create user
+  const user = buildUser(overrides)
+
+  cy.request({
+    url: 'http://localhost:3000/register',
+    method: 'POST',
+    body: user,
+  }).then(response => ({...response.body.user, ...user}))
+})
