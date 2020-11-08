12861c30b84beb1567cb10d6cb84206f2337761b
infantito <danielinfantito61@gmail.com>
2020-11-07 19:02:00 -0500

    feat: cypress driven development

--- 06-jest-cypress-react-babel-webpack/cypress/e2e/register.js
+++ 06-jest-cypress-react-babel-webpack/cypress/e2e/register.js
@@ -20,5 +20,7 @@ describe('registration', () => {
     cy.window()
       .its('localStorage.token')
       .should('be.a', 'string')
+
+    cy.findByTestId('username-display').should('have.text', user.username)
   })
 })
