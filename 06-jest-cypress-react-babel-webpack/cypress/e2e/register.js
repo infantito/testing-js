52ebcf621ac97222f5a09bfe2a3dc95fcd7ef587
infantito <danielinfantito61@gmail.com>
2020-11-07 19:17:40 -0500

    feat: simulate http errors in cypress tests

--- 06-jest-cypress-react-babel-webpack/cypress/e2e/register.js
+++ 06-jest-cypress-react-babel-webpack/cypress/e2e/register.js
@@ -23,4 +23,21 @@ describe('registration', () => {
 
     cy.findByTestId('username-display').should('have.text', user.username)
   })
+
+  it(`should show an error message if there's an error registering`, () => {
+    cy.server()
+
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/register',
+      status: 500,
+      response: {},
+    })
+
+    cy.visit('/register')
+
+    cy.findByText(/submit/i).click()
+
+    cy.findByText(/error.*try again/i)
+  })
 })
