1b5178ec008b03ec1e6c93c3fbf8c089aec3e15c
infantito <danielinfantito61@gmail.com>
2020-11-11 21:27:29 -0500

    feat: use dom rtl with svelte

--- 07-dom-testing-library-with-anything/svelte.test.js
+++ 07-dom-testing-library-with-anything/svelte.test.js
@@ -1,7 +1,8 @@
 import '@testing-library/jest-dom/extend-expect'
 import {userEventAsync} from './user-event-async'
-import {getQueriesForElement} from '@testing-library/dom'
+import {getQueriesForElement, waitFor} from '@testing-library/dom'
 import Counter from './counter.svelte'
+import userEvent from '@testing-library/user-event'
 
 function render(Component) {
   const container = document.createElement('div')
@@ -16,10 +17,14 @@ function render(Component) {
 
 test('counter increments', async () => {
   const {getByText} = render(Counter)
-  const counter = getByText('0')
-  await userEventAsync.click(counter)
-  expect(counter).toHaveTextContent('1')
 
-  await userEventAsync.click(counter)
-  expect(counter).toHaveTextContent('2')
+  const counter = getByText('0')
+
+  userEvent.click(counter)
+
+  await waitFor(() => expect(counter).toHaveTextContent('1'))
+
+  userEvent.click(counter)
+
+  await waitFor(() => expect(counter).toHaveTextContent('2'))
 })
